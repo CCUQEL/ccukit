7881874afba2dff5f7d1dd0b1e0a5c55f799ba75
CCUQEL <ccuqel@gmail.com>
2025-03-01 23:19:24 +0800

    add set_plot_style() as general tool

--- general.py
+++ general.py
@@ -15,6 +15,7 @@ import numpy as np
 __all__ = [
     'get_path',
     'plot_trace',
+    'set_plot_style',
 ]
 
 def get_path(ext: str, title = 'Select a file path', save_file = False):
@@ -78,3 +79,94 @@ def plot_trace(freq, signal):
     # Adjust layout and show the plot
     plt.tight_layout()
     plt.show()
+
+def set_plot_style():
+    """Modify the plot style of matplotlib. I don't like default one. 
+
+    It also returns a dictionary to set marker as a hollow one.
+
+    Example:
+    >>> from matplotlib import pyplot as plt
+    >>> hms = set_plot_style()
+    >>> plt.plot([0], [0], 'o', **hms)
+    >>> plt.show()
+    """
+    plt.rcParams.update({
+        #### Font settings
+        'font.family': 'Times New Roman',
+        'mathtext.fontset': 'cm',
+        'mathtext.rm': 'serif',
+        'mathtext.it': 'serif:italic',
+        'mathtext.bf': 'serif:bold',
+        'axes.labelsize': 15,
+        'axes.titlesize': 20,
+        # pad
+        'axes.titlepad': 10,  # Padding for title
+        # for xy label pad, write labelpad=pad as parm.
+
+        #### Ticks
+        # general
+        'xtick.direction': 'out',
+        'ytick.direction': 'out',
+        # tick on/off
+        'xtick.bottom': True,
+        'xtick.top': False,
+        'ytick.left': True,
+        'ytick.right': False,
+        # tick-label on/off
+        'xtick.labelbottom': True,
+        'xtick.labeltop': False,
+        'ytick.labelleft': True,
+        'ytick.labelright': False,
+        # about major-ticks
+        'xtick.major.size': 8,
+        'ytick.major.size': 8,
+        'xtick.labelsize': 14,  # fontsize
+        'ytick.labelsize': 14,  # fontsize
+        # about mirror-ticks (plt.minorticks_on())
+        'xtick.minor.visible': True,
+        'ytick.minor.visible': True,
+        'xtick.minor.size': 3,
+        'ytick.minor.size': 3,
+        #'xtick.minor.ndivs': ['auto', 4][1],
+        #'ytick.minor.ndivs': ['auto', 4][1],
+
+        #### legend
+        'legend.fontsize': 11,
+        'legend.title_fontsize': 18, 
+        'legend.labelspacing': 0.4,
+        'legend.frameon': True,
+        'legend.handletextpad': 0.2,
+        'legend.loc': ['best', 0][0], # 0~10
+        'legend.markerscale': 2.0,
+
+        #### axes
+        # general
+        'axes.linewidth': 0.8,
+        # spines
+        'axes.spines.bottom': True,
+        'axes.spines.left': True,
+        'axes.spines.right': True,
+        'axes.spines.top': True,
+        # margin (xy lim, percentage)
+        'axes.xmargin': 0.0,  
+        'axes.ymargin': 0.05, 
+        'axes.zmargin': 0.05, 
+
+        # grid
+        'axes.grid': False,
+        'axes.grid.axis': ['both', 'x', 'y'][0],
+        'axes.grid.which': ['major', 'minor', 'both'][0],
+        'grid.alpha': 0.3, # transparency
+        'grid.color': '#000000',
+        'grid.linestyle': '-',
+        'grid.linewidth': 0.8,
+    })
+
+    hollow_marker_setting = {
+        'markerfacecolor': 'none',
+        'markersize': 6, 
+        'markeredgewidth': 1.5
+    }
+
+    return hollow_marker_setting
