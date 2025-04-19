5542b6d70f249fd597bd31232c127d463cc399fb
CCUQEL <ccuqel@gmail.com>
2025-04-19 12:56:36 +0800

    make it faster by not saving screenshot file.

--- visadriver/tm901pro.py
+++ visadriver/tm901pro.py
@@ -8,6 +8,8 @@ from PIL import Image
 import pyautogui
 import cv2
 import os
+import numpy as np
+import pygetwindow as gw
 
 # Load digit templates into a dictionary.
 script_directory = os.path.dirname(os.path.abspath(__file__))
@@ -22,7 +24,6 @@ __all__ = [
     'TM901pro'
 ]
 
-import pygetwindow as gw
 class TM901pro():
     """Need to open TM-901pro software first. Set to DC*10 mode.
 
@@ -62,22 +63,21 @@ class TM901pro():
     def read_mag_field_mT(self):
         """Read the mearued magnetic field (mT). N is negative, S is positive."""
         # screen shot the measured value
-        screenshot_region((150, 310, 380, 120), 'value.png')
+        value_img = screenshot_region((150, 310, 380, 120))
 
         # screen shot the N/S pixel
-        screenshot_region((170, 265, 1, 1), 'NS_pixel.png')
+        ns_pixel_img = screenshot_region((170, 265, 1, 1))
 
         # cut the value into four each digits, e+2~e-2, then convert to string then float
-        img = Image.open('value.png')
-        ep2_str = recognize_numerical_value( img.crop((  0, 0,  70, 120)) )
-        ep1_str = recognize_numerical_value( img.crop(( 70, 0, 140, 120)) )
-        ep0_str = recognize_numerical_value( img.crop((140, 0, 210, 120)) )
-        em1_str = recognize_numerical_value( img.crop((237, 0, 307, 120)) )
-        em2_str = recognize_numerical_value( img.crop((307, 0, 377, 120)) )
+        ep2_str = recognize_numerical_value( value_img.crop((  0, 0,  70, 120)) )
+        ep1_str = recognize_numerical_value( value_img.crop(( 70, 0, 140, 120)) )
+        ep0_str = recognize_numerical_value( value_img.crop((140, 0, 210, 120)) )
+        em1_str = recognize_numerical_value( value_img.crop((237, 0, 307, 120)) )
+        em2_str = recognize_numerical_value( value_img.crop((307, 0, 377, 120)) )
         value = float(ep2_str + ep1_str + ep0_str + '.' + em1_str + em2_str)
 
         # extract the N/S
-        pixel_color = get_pixel_color('NS_pixel.png')
+        pixel_color = get_pixel_color(ns_pixel_img)
         if pixel_color == (255, 0, 0): # S
             pass
         else: # N
@@ -85,7 +85,6 @@ class TM901pro():
 
         return value
 
-import numpy as np
 def recognize_numerical_value(img):
     """Recognize digit using template matching."""
     img = img.convert("L")  # Convert to grayscale (if not already)
@@ -104,19 +103,17 @@ def recognize_numerical_value(img):
     return str(best_match)
 
 
-def screenshot_region(region, output_path):
+def screenshot_region(region, output_path='') -> Image:
     """Takes a screenshot of a specified region of the screen and saves it to a file.
 
     region has formate (left, top, width, height)
     """
     screenshot = pyautogui.screenshot(region=region)
-    screenshot.save(output_path)
+    if output_path != '':
+        screenshot.save(output_path)
+    return screenshot
 
-def get_pixel_color(image_path):
+def get_pixel_color(img):
     # Open the image using Pillow
-    img = Image.open(image_path)
-    
-    # Get the color of the single pixel
     pixel_color = img.getpixel((0, 0))
-
     return pixel_color
